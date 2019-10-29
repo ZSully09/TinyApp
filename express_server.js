d98210926a7764c959ef6c11e5356242948eefeb
ZSully09 <zsullivan93@gmail.com>
2019-10-29 17:08:05 +0000

    initial url shortening commit

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; // default
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +11,16 @@ const urlDatabase = {
   Goog: 'http://www.google.com',
   TSN: 'https://www.tsn.ca'
 };
+
+function generateRandomString() {
+  let result = '';
+  let characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -26,6 +38,21 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
