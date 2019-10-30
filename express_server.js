d31172830ef025a1e716ac0715cf129a6d456fc3
ZSully09 <zsullivan93@gmail.com>
2019-10-30 00:17:29 +0000

    addition of edit button to change the long url associated with a specific short url

--- express_server.js
+++ express_server.js
@@ -60,6 +60,13 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/urls/:shortURL', (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
