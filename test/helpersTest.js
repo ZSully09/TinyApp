5b7765f1178651c7c46a5a658b3f91b7d9cbe550
ZSully09 <zsullivan93@gmail.com>
2019-11-01 22:09:22 +0000

    created tests to ensure the getUserByEmail returns the correct result (for user id, undefined if invalid email, email, and password

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -6,19 +6,36 @@ const testUsers = {
   userRandomID: {
     id: 'userRandomID',
     email: 'user@example.com',
-    password: 'purple-monkey-dinosaur'
+    password: 'password'
   },
   user2RandomID: {
     id: 'user2RandomID',
     email: 'user2@example.com',
-    password: 'dishwasher-funk'
+    password: 'password2'
   }
 };
 
-describe('getUserByEmail', function() {
-  it('should return a user with valid email', function() {
-    const user = getUserByEmail('user@example.com', users);
-    const expectedOutput = 'userRandomID';
-    // Write your assert statement here
+describe('getUserByEmail', function () {
+  it('should return a user with valid email', function () {
+    const user = getUserByEmail('user@example.com', testUsers);
+    const expectedOutputId = 'userRandomID';
+    assert.equal(user.id, expectedOutputId);
+  });
+
+  it('should return a undefined with invalid email', function () {
+    const user = getUserByEmail('u@example.com', testUsers);
+    assert.equal(user, undefined);
+  });
+
+  it('should return a email with valid email', function () {
+    const emailTest = 'user@example.com';
+    const user = getUserByEmail(emailTest, testUsers);
+    assert.equal(user.email, emailTest);
+  });
+
+  it('should return a password with valid email', function () {
+    const user = getUserByEmail('user2@example.com', testUsers);
+    const expectedOutputPassword = 'password2';
+    assert.equal(user.password, expectedOutputPassword);
   });
 });
